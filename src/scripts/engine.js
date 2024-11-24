173ab1197309f40bb406080fc4ae5249dc4d0a57
Alex Sandro <lexilva@gmail.com>
2024-11-24 11:16:28 -0300

    Implentação do Score finalizada

---
+++ src/scripts/engine.js
@@ -0,0 +1,50 @@
+    const state ={
+        view:{
+            squares: document.querySelectorAll(".square"),
+            enemy: document.querySelectorAll(".enemy"),
+            timeLeft: document.querySelector("#time-lef"),
+            score: document.querySelector("#score")
+
+        },
+        values:{
+            timerId:null,
+            gameVelocity:900,
+            hitPosition:0,
+            result:0
+        }
+    };
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare= state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id
+
+}
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+}
+
+    function addListeneHitBox(){
+        state.view.squares.forEach((square =>{
+            square.addEventListener("mousedown",()=>{
+                if (square.id === state.values.hitPosition){
+                    state.values.result++;
+                    state.view.score.textContent = state.values.result;
+                    state.values.hitPosition = null;
+                }
+            })
+        }))
+    }
+
+    function initialize(){
+        
+        moveEnemy();
+        addListeneHitBox();
+        
+      }
+
+    initialize();
